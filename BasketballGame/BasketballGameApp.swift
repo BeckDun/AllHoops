57b0737ac6233d8c3f1034090842c6210161c5c9
Beckett Dunlavy <beckett.dunlavy@gmail.com>
2025-06-05 16:23:47 -0600

    Initial Commit

---
+++ BasketballGame/BasketballGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketballGameApp.swift
+//  BasketballGame
+//
+//  Created by Beckett Dunlavy on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BasketballGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
